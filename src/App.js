9e7992303a3602a83077494ba48c508faef7ce6a
HMP1993 <mr.momen.mrm@gmail.com>
2022-11-23 20:08:28 +0000

    Time and location comp added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import UilReact from "@iconscout/react-unicons/icons/uil-react";
 import "./App.css";
 import Input from "./components/Input";
+import TimeAndLocation from "./components/TimeAndLocation";
 import TopButtons from "./components/TopButtons";
 
 function App() {
@@ -8,6 +9,8 @@ function App() {
     <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400 ">
       <TopButtons />
       <Input />
+
+      <TimeAndLocation/>
     </div>
   );
 }
--- src/components/TimeAndLocation.js
+++ src/components/TimeAndLocation.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 function TimeAndLocation() {
-  return <div>TimeAndLocation</div>;
+  return (
+    <div>
+      <div className=" flex items-center justify-center my-6">
+        <p className="text-white text-xl font-extralight">
+          20:03 Wednesday, 23 November 2022 (GMT)
+        </p>
+      </div>
+      <div className=" flex items-center justify-center my-3">
+        <p className=" text-white text-xl font-medium ">Canterbury, UK</p>
+      </div>
+    </div>
+  );
 }
 
 export default TimeAndLocation;
