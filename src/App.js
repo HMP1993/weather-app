b6abb246884a9651031a3e8371447d6160473bef
HMP1993 <mr.momen.mrm@gmail.com>
2022-11-23 18:43:37 +0000

    added tailwind and unicorn

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-
-import './App.css';
+import UilReact from "@iconscout/react-unicons/icons/uil-react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hi!</h1>
+      <h1 className="text-2xl font-bold text-red-500 ">Hi!</h1>
+      <UilReact></UilReact>
     </div>
   );
 }
