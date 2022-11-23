775967997a66a2940c582e3b18f223e46770efa3
HMP1993 <mr.momen.mrm@gmail.com>
2022-11-23 21:06:56 +0000

    last commit for today

---
+++ src/components/TempratureAndDetails.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function TempratureAndDetails() {
+  return (
+    <div>
+      <div className=" flex items-center justify-center py-6 text-xl to-cyan-300">
+        <p>Windy</p>
+      </div>
+      <div className=" flex flex-row items-center justify-between text-white py-3">
+        <img src=""/>
+      </div>
+    </div>
+  );
+}
+
+export default TempratureAndDetails;
