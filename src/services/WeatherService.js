7fe2b86f3e0f05bb3faeb9ec9e2c9aaefd2168fb
HMP1993 <mr.momen.mrm@gmail.com>
2022-11-27 16:14:06 +0000

    data formated peoperly

--- src/services/WeatherService.js
+++ src/services/WeatherService.js
@@ -2,28 +2,47 @@ const API_KEY = "8e92b13b467afad35c2f32718beeff31";
 const BASE_URL = "https://api.openweathermap.org/data/2.5";
 
 const getWeatherData = (infoType, searchParams) => {
+  const url = new URL(BASE_URL + "/" + infoType);
+  url.search = new URLSearchParams({ ...searchParams, appid: API_KEY });
 
-    const url = new URL(BASE_URL +'/'+ infoType);
-    url.search = new URLSearchParams({...searchParams, appid:API_KEY})
-
-    return fetch(url).then((res) => res.json())
+  return fetch(url).then((res) => res.json());
 };
-    const formatCurrentWeather = (data) => {
-        const {
-                coord:{lon,lat},
-                main:{temp, feels_like, temp_min, temp_max, humidity},
-                name,
-                dt,
-                sys:{country,sunrise,sunset,},
-                weather,
-                wind:{speed}
-                
-            
-        } = data
-        const {main:details, icon} = weather[0];
-        return{lon,lat,temp,feels_like,temp_max,temp_min,humidity,name,dt,country,sunrise,sunset,details,icon,speed}
-    };
+const formatCurrentWeather = (data) => {
+  const {
+    coord: { lon, lat },
+    main: { temp, feels_like, temp_min, temp_max, humidity },
+    name,
+    dt,
+    sys: { country, sunrise, sunset },
+    weather,
+    wind: { speed },
+  } = data;
+  const { main: details, icon } = weather[0];
+  return {
+    lon,
+    lat,
+    temp,
+    feels_like,
+    temp_max,
+    temp_min,
+    humidity,
+    name,
+    dt,
+    country,
+    sunrise,
+    sunset,
+    details,
+    icon,
+    speed,
+  };
+};
 
-const getFormattedWeatherData = (searchParams) => {
-    const formattedCurrentWeather = await getWeatherData ('weather', searchParams).then(formatCurrentWeather)
-}
+const getFormattedWeatherData = async (searchParams) => {
+  const formattedCurrentWeather = await getWeatherData(
+    "weather",
+    searchParams
+  ).then(formatCurrentWeather);
+
+  return formattedCurrentWeather;
+};
+export default getFormattedWeatherData;
